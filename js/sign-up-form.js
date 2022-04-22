5c0e9419d474f17e1a340269b5df8f53ccda716d
Justin <Justin.nthai@gmail.com>
2022-04-21 18:21:44 -0700

    Restructure html form and button along with corresponding styles and classes, Add button animations, and form input requirements, Initiate basic variables in js

---
+++ js/sign-up-form.js
@@ -0,0 +1,7 @@
+const password1 = document.querySelector('[data-pw-1]');
+const password2 = document.querySelector('[data-pw-1]');
+
+/* something like when active? or focus, call this fucntion, put some pattern checks on email, and phone in html
+   put some divs for the password errors to show too, and if you want extra, you can add the checkmarks to the fields */
+
+passwordMatch(password1, password2) => () 
