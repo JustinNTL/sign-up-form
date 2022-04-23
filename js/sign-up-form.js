f632dda9e8c81f7676abaef81300e7eeda443446
Justin <Justin.nthai@gmail.com>
2022-04-22 23:34:24 -0700

    Add css custom property variables to manipulate pseudo elements in js and data-form attribute to form, Lower css input specificity to add input error class in js, Write function for password matching confirmation on keyup event, Start on function for other required inputs possibility

--- js/sign-up-form.js
+++ js/sign-up-form.js
@@ -4,17 +4,32 @@ const lastName = document.querySelector('[data-last-name]');
 const email = document.querySelector('[data-email]');
 const phone = document.querySelector('[data-phone]');
 const password1 = document.querySelector('[data-pw-1]');
-const password2 = document.querySelector('[data-pw-1]');
+const password2 = document.querySelector('[data-pw-2]'); 
 const submitButton = document.querySelector('button[type=submit]')
+const form = document.querySelector('[data-form]');
 
-/* use .input-error and add as a class and toggle visibility on .req-check::after, and .pw-check::after */
+/* use .input-error and add as a class and toggle visibility on .req-check::after */
 
-/* while(password1 !== null && password2 !== null) {
-   if (password1 === password2) {
+const requiredCheck = () => {
+   if (firstName.value == '') {
+      document.documentElement.style.setProperty('--reqVisibility', 'visible');
 
    }
 }
 
-passwordMatch(password1, password2) = () => {
+const passwordMatch = () => {
+   if (password1.value !== '' && password2.value !== '' && password1.value !== password2.value) {
+      document.documentElement.style.setProperty('--pwVisibility', 'visible');
+      password1.classList.add('input-error');
+      password2.classList.add('input-error');
+   } else {
+      document.documentElement.style.setProperty('--pwVisibility', 'hidden');
+      password1.classList.remove('input-error');
+      password2.classList.remove('input-error');
+   }
+}
 
-} */
+password1.onkeyup = passwordMatch;
+password2.onkeyup = passwordMatch;
+
+form.onsubmit = requiredCheck;
