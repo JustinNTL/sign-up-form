728eb233e64cd33691535a919ccd7746cf788aa1
Justin <Justin.nthai@gmail.com>
2022-04-24 17:43:03 -0700

    Expand on js logic with breakdown of more css custom variables for each input and separate html classes, Set data attribute for submit button for js, Create respective functions for each input check, Add event to each input keyup and button submit use case

--- js/sign-up-form.js
+++ js/sign-up-form.js
@@ -1,20 +1,70 @@
-
 const firstName = document.querySelector('[data-first-name]');
 const lastName = document.querySelector('[data-last-name]');
 const email = document.querySelector('[data-email]');
 const phone = document.querySelector('[data-phone]');
 const password1 = document.querySelector('[data-pw-1]');
 const password2 = document.querySelector('[data-pw-2]'); 
-const submitButton = document.querySelector('button[type=submit]')
-const form = document.querySelector('[data-form]');
+const submitButton = document.querySelector('[data-btn]');
 
-/* use .input-error and add as a class and toggle visibility on .req-check::after */
-
-const requiredCheck = () => {
-   if (firstName.value == '') {
-      document.documentElement.style.setProperty('--reqVisibility', 'visible');
-
+const fnReqCheck = () => {
+   if (firstName.value === '') {
+      document.documentElement.style.setProperty('--fnReqVisibility', 'visible');
+      firstName.classList.add('input-error');
+	 }  else {
+			document.documentElement.style.setProperty('--fnReqVisibility', 'hidden');
+			firstName.classList.remove('input-error');
    }
+}
+
+const lnReqCheck = () => {
+   if (lastName.value === '') {
+      document.documentElement.style.setProperty('--lnReqVisibility', 'visible');
+      lastName.classList.add('input-error');
+   } 	else {
+      document.documentElement.style.setProperty('--lnReqVisibility', 'hidden');
+      lastName.classList.remove('input-error');
+   }
+}
+
+const elReqCheck = () => {
+   if (email.value === '') {
+      document.documentElement.style.setProperty('--elReqVisibility', 'visible');
+      email.classList.add('input-error');
+   } 	else {
+      document.documentElement.style.setProperty('--elReqVisibility', 'hidden');
+      email.classList.remove('input-error');
+   }
+}
+
+const phReqCheck = () => {
+   if (phone.value === '') {
+      document.documentElement.style.setProperty('--phReqVisibility', 'visible');
+      phone.classList.add('input-error');
+   } 	else {
+      document.documentElement.style.setProperty('--phReqVisibility', 'hidden');
+      phone.classList.remove('input-error');
+   }
+}
+
+/* Notice double quotes on '"* Required"' to change the custom css string variable */
+
+const pwReqCheck = () => {
+   if (password1.value === '') {
+      document.documentElement.style.setProperty('--pwMatch', '"* Required"');
+      document.documentElement.style.setProperty('--pwVisibility', 'visible');
+      password1.classList.add('input-error');
+   } 	else {
+      document.documentElement.style.setProperty('--pwMatch', '"* Passwords do not match"');
+   }
+}
+
+const pwConfReqCheck = () => {
+   if (password2.value === '') {
+      document.documentElement.style.setProperty('--confPwVisibility', 'visible');
+      password2.classList.add('input-error');
+   } 	else {
+			document.documentElement.style.setProperty('--confPwVisibility', 'hidden');
+	 }
 }
 
 const passwordMatch = () => {
@@ -22,14 +72,45 @@ const passwordMatch = () => {
       document.documentElement.style.setProperty('--pwVisibility', 'visible');
       password1.classList.add('input-error');
       password2.classList.add('input-error');
-   } else {
+   } 	else {
       document.documentElement.style.setProperty('--pwVisibility', 'hidden');
       password1.classList.remove('input-error');
       password2.classList.remove('input-error');
    }
 }
 
-password1.onkeyup = passwordMatch;
-password2.onkeyup = passwordMatch;
+/* Show error on first submit, but remove during user fix */
 
-form.onsubmit = requiredCheck;
+submitButton.addEventListener('click', () => {
+  fnReqCheck();
+	lnReqCheck();
+	elReqCheck();
+	phReqCheck();
+	pwReqCheck();
+	pwConfReqCheck();
+});
+
+firstName.addEventListener('keyup', fnReqCheck);
+
+lastName.addEventListener('keyup', lnReqCheck);
+
+email.addEventListener('keyup', elReqCheck);
+
+phone.addEventListener('keyup', phReqCheck);
+
+password1.addEventListener('keyup', () => {
+  passwordMatch();
+	pwReqCheck();
+});
+
+password2.addEventListener('keyup', () => {
+	passwordMatch();
+	pwConfReqCheck();
+});
+
+/* Other "less readable" way to call method on event
+
+password1.onkeyup = passwordMatch;
+password2.onkeyup = passwordMatch; 
+
+Notice the difference of 'keyup' vs 'onkeyup' */
